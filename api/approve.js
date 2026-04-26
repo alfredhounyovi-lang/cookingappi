47fd267e45cd807b6f8675027d0e44aefb96a7a9
alfredhounyovi-lang <alfredhounyovi@gmail.com>
2026-04-26 13:12:55 +0100

    Update approve.js

--- api/approve.js
+++ api/approve.js
@@ -1,28 +1,20 @@
 export default async function handler(req, res) {
-  try {
-    if (req.method !== "POST") {
-      return res.status(405).send("Method Not Allowed");
-    }
+  if (req.method !== "POST") return res.status(405).send("Method Not Allowed");
 
+  try {
     const { paymentId } = req.body;
 
-    const response = await fetch(
-      `https://api.minepi.com/v2/payments/${paymentId}/approve`,
-      {
-        method: "POST",
-        headers: {
-          Authorization: `Key ${process.env.PI_API_KEY}`
-        }
+    const response = await fetch(`https://minepi.com{paymentId}/approve`, {
+      method: "POST",
+      headers: { 
+        "Authorization": `Key ${process.env.PI_API_KEY}`,
+        "Content-Type": "application/json"
       }
-    );
+    });
 
     const data = await response.json();
-    console.log("APPROVE:", data);
-
     return res.status(200).json(data);
-
   } catch (error) {
-    console.error(error);
-    return res.status(500).json({ error: "approve failed" });
+    return res.status(500).json({ error: "Approve failed" });
   }
 }
