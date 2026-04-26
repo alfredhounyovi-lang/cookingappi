e9f3a76dd62608dd9a50ada91d922407af0719df
alfredhounyovi-lang <alfredhounyovi@gmail.com>
2026-04-26 12:42:44 +0100

    Update complete.js

--- api/complete.js
+++ api/complete.js
@@ -6,20 +6,23 @@ export default async function handler(req, res) {
 
     const { paymentId } = req.body;
 
-    const response = await fetch(`https://api.minepi.com/v2/payments/${paymentId}/complete`, {
-      method: "POST",
-      headers: {
-        "Authorization": "1gfftilc2c1hl2vi5fhhyfn3a84nkb5nm4slow6rgmnktyvrjaqjshy13qoqhban"
+    const response = await fetch(
+      `https://api.minepi.com/v2/payments/${paymentId}/complete`,
+      {
+        method: "POST",
+        headers: {
+          Authorization: `Key ${process.env.PI_API_KEY}`
+        }
       }
-    });
+    );
 
     const data = await response.json();
-    console.log("Complete response:", data);
+    console.log("COMPLETE:", data);
 
     return res.status(200).json(data);
 
   } catch (error) {
     console.error(error);
-    return res.status(500).json({ error: "fail" });
+    return res.status(500).json({ error: "complete failed" });
   }
 }
